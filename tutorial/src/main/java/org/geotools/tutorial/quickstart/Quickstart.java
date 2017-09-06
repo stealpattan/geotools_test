8e828561a0a10a99aceb313d954bfb4c710684ce
stealpattan <stealpattan@gmail.com>
2017-09-06 12:37:57 +0900

    line color change

--- tutorial/src/main/java/org/geotools/tutorial/quickstart/Quickstart.java
+++ tutorial/src/main/java/org/geotools/tutorial/quickstart/Quickstart.java
@@ -10,6 +10,7 @@ import org.geotools.data.DataUtilities;
 import org.geotools.data.FileDataStore;
 import org.geotools.data.FileDataStoreFinder;
 import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.feature.DefaultFeatureCollection;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
@@ -17,12 +18,14 @@ import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.Layer;
 import org.geotools.map.MapContent;
-import org.geotools.styling.SLD;
-import org.geotools.styling.Style;
+import org.geotools.styling.*;
+import org.geotools.styling.Stroke;
 import org.geotools.swing.JMapFrame;
 import org.geotools.swing.data.JFileDataStoreChooser;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.GeometryDescriptor;
+import org.opengis.filter.FilterFactory;
 
 
 /**
@@ -64,7 +67,25 @@ public class Quickstart {
         SimpleFeature simpleFeature = sfb.buildFeature(null);
         lineCollection.add(simpleFeature);
 
-        Layer lineLayer = new FeatureLayer(lineCollection,style);
+        StyleFactory sf = CommonFactoryFinder.getStyleFactory(null);
+        FilterFactory ff = CommonFactoryFinder.getFilterFactory2(null);
+        Stroke stroke = sf.createStroke(
+                ff.literal(new Color(0xC8, 0x46, 0x63)),
+                ff.literal(3)
+        );
+
+        GeometryDescriptor geomdesc = featuresource.getSchema().getGeometryDescriptor();
+        String hoe = geomdesc.getLocalName();
+        LineSymbolizer LS = sf.createLineSymbolizer(stroke,hoe);
+
+        Rule rule = sf.createRule();
+        rule.symbolizers().add(LS);
+        Rule rules[] = {rule};
+        FeatureTypeStyle fts = sf.createFeatureTypeStyle(rules);
+        Style style2 = sf.createStyle();
+        style2.featureTypeStyles().add(fts);
+
+        Layer lineLayer = new FeatureLayer(lineCollection,style2);
 
         map.addLayer(lineLayer);
 
